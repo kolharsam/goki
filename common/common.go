73623fe239d5dd610596a4fb34546405622818fd
Sameer Kolhar <kolhar730@gmail.com>
2020-06-14 21:56:38 +0530

    change response type

--- common/common.go
+++ common/common.go
@@ -1,13 +1,15 @@
 package common
 
+import "time"
+
 // GokiRequest is for all requests
 type GokiRequest struct {
-	Command string `json:"command"`
-	Args []string `json:"args"`
+	Command string   `json:"command"`
+	Args    []string `json:"args"`
 }
 
 // GokiResponse is for all responses
 type GokiResponse struct {
-	Result string `json:"result"`
-	TimeStamp string `json:"timestamp"`
+	Result    string    `json:"result"`
+	TimeStamp time.Time `json:"timestamp"`
 }
